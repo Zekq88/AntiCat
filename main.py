a02992ca7e773f08bf92908b3ff9e0a6929ae4b1
Robin Kadergran <titi_malaki@live.se>
2025-06-09 18:38:28 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+
+import threading
+import pygame
+import time
+
+cat = 15
+# the model of yolo
+model = YOLO("yolov8n.pt")
+
+# loads the model version
+results = model.predict(source=0, stream=True, show=True)
+
+# plays the sirens
+def sirens():
+   # global sirenTime
+    pygame.mixer.init()
+    pygame.mixer.music.load("Sirens.mp3")
+    pygame.mixer.music.play()
+
+# iteration from webcam
+for result in results:
+    
+    boxes = result.boxes
+    names = result.names
+   
+    print("CLS baby! " + str(boxes.cls))
+    # för att se alla värden
+    # print(str(names).replace(",", "\n"))
+    if cat in boxes.cls:
+        thread = threading.Thread(target=sirens, daemon=True)
+        print("\033[0;31m" "Alert: A Cat has been detected!!!")
+        print("Sound the sirens!!!" + "\033[0m")
+        thread.start()
+        time.sleep(20)
